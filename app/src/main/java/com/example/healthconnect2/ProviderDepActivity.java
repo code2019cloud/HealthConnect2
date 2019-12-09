8ab78a763bfb0ad6ff626655be448cca2418379f
code2019cloud <code2019.cloud@gmail.com>
2019-12-09 19:23:18 +0530

    first commit

---
+++ app/src/main/java/com/example/healthconnect2/ProviderDepActivity.java
@@ -0,0 +1,17 @@
+package com.example.healthconnect2;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ProviderDepActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_providerdep);
+    }
+
+
+
+
+    }
---
+++ app/src/main/java/com/example/healthconnect2/ProviderDrActivity.java
@@ -0,0 +1,19 @@
+package com.example.healthconnect2;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ProviderDrActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_providerdr);
+    }
+
+
+
+
+    }
