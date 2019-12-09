9de11828e6af3dd2f6bab63f6d84d683a66572a0
code2019cloud <code2019.cloud@gmail.com>
2019-12-09 19:15:31 +0530

    first commit

--- app/src/main/java/com/example/healthconnect2/ProviderSigninActivity.java
+++ app/src/main/java/com/example/healthconnect2/ProviderSigninActivity.java
@@ -3,6 +3,8 @@ package com.example.healthconnect2;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -15,13 +17,23 @@ public class ProviderSigninActivity extends AppCompatActivity {
     }
 
     public void patientSigninLinkClick(View view){
-        //make database connection and validate
-        //if success show homescreen or display msg and show the same screen
-        Intent intent=new Intent(this, PatientHomeActivity.class);
-        startActivity(intent);
-    }
+            //make database connection and validate
+            //if success show homescreen or display error msg and show the same screen
+            final EditText etusername = findViewById(R.id.prouser);
+            final EditText etpassword = findViewById(R.id.propass);
+            String prousername = etusername.getText().toString();
+            String propassword = etpassword.getText().toString();
+            if (prousername.equals("user") && propassword.equals("pass")) {
+                Toast.makeText(getApplicationContext(), "Success", Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(this, ProviderProfileActivity.class);
+                // Intent intent = new Intent(getApplicationContext(), PatientSearchMainActivity.class);
+                startActivity(intent);
+            } else {
+                Toast.makeText(getApplicationContext(), "Invalid Credentials", Toast.LENGTH_LONG).show();
+            }
+        }
 
-    public void patientSignupLinkClick(View view){
+    public void providerSignupLinkClick(View view){
         Intent intent=new Intent(this, PatientSignupActivity.class);
         startActivity(intent);
     }
